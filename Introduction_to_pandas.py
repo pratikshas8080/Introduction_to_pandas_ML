25652489a051972728eef5028fc961752fc5312d
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2020-08-31 11:50:46 +0530

    Pandas_ML_All_Basic_Information_with_module

---
+++ Introduction_to_pandas.py
@@ -0,0 +1,7 @@
+# Intro to pandas
+# Learning Objectives:
+
+# 1. Gain an introduction to the DataFrame and Series data structures of the pandas library
+# 2. Access and manipulate data within a DataFrame and Series
+# 3. Import CSV data into a pandas DataFrame
+# 4. Reindex a DataFrame to shuffle data
